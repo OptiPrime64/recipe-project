afcab871a73c2871080e562d1ca4e857fb2eb363
Harold Jennett <haroldjennett@outlook.com>
2020-08-11 12:06:19 -0400

    Finished routing section

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnDestroy {
   recipe: Recipe;
   id: number;
+  paramsSubscription: Subscription;
 
   constructor(private recipeService: RecipeService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   
 
@@ -22,14 +25,34 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.id = +params ['id'];
         this.recipe = this.recipeService.getRecipe(this.id);
       }
     )
   }
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
+
+
+
 }
+
+
+
+
+//   onEditRecipe(){
+//     this.router.navigate(['edit'], {relativeTo: this.route});
+
+// OR
+
+//     this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+//   }
+
+
+
 
 // MY SOLUTION
 
